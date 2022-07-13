a8c1ba8ba93de2cf24ee3105529e100525727de3
tati2327 <tatumora1302@gmail.com>
2022-07-12 22:50:20 -0600

    Subir archivos faltantes

--- app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/HomeActivity.java
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.pnp.galia_app_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 }
--- app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/LoginActivity.java
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/LoginActivity.java
@@ -17,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
         btnLogin = (Button) findViewById(R.id.login_button);
         editTextUser = findViewById(R.id.username);
