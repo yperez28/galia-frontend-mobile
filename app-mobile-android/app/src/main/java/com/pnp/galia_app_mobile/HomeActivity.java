3d12e314f7b394303f912075fb485e1db6005e24
Yerling Pérez Álvarez <perez29yerling@gmail.com>
2022-07-29 23:33:20 -0600

    Copy menu files
    Copy files from external project created by Keyner and Daniel

--- app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/HomeActivity.java
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/HomeActivity.java
@@ -1,17 +1,14 @@
 package com.pnp.galia_app_mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.TextView;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -42,9 +39,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         floating_button_main = findViewById(R.id.floating_button_main);
         floating_button_appointment = findViewById(R.id.floating_button_appointment);
---
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.pnp.galia_app_mobile.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
