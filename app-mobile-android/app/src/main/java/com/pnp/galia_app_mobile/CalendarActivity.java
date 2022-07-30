b688fe454c13c5da645960a9ec871b0c91447662
Yerling Pérez Álvarez <perez29yerling@gmail.com>
2022-07-29 18:06:35 -0600

    Implement calendar view
    - Create calendar implementation
    - Create day event view

---
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/CalendarActivity.java
@@ -0,0 +1,101 @@
+package com.pnp.galia_app_mobile;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class CalendarActivity extends AppCompatActivity implements OnDateSelectedListener {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEEE d", new Locale("es", "ES"));
+
+    @BindView(R.id.calendarView)
+    MaterialCalendarView calendar;
+    @BindView(R.id.selected_day)
+    TextView selectedDay;
+    @BindView(R.id.add_task)
+    RelativeLayout addTaskHolder;
+    @BindView(R.id.add_task_button)
+    Button addTaskButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        ButterKnife.bind(this);
+
+        LinearLayout header = findViewById(com.prolificinteractive.materialcalendarview.R.id.header);
+        header.setBackgroundResource(R.drawable.ic_top_calendar);
+        header.setPadding(120,0,120,0);
+
+        final LocalDate instance = LocalDate.now();
+        calendar.setSelectedDate(instance);
+        calendar.setOnDateChangedListener(this);
+
+        addTaskButton.setOnClickListener(view -> {
+            String message = "Adding new task...";
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        });
+    }
+
+    @Override
+    public void onDateSelected(
+            @NonNull MaterialCalendarView widget,
+            @NonNull CalendarDay date,
+            boolean selected) {
+        String day = FORMATTER.format(date.getDate());
+        String finalDay = day.substring(0, 1).toUpperCase() + day.substring(1);
+
+        selectedDay.setText(selected ? finalDay : "Seleccione una fecha");
+
+        addTaskHolder.setVisibility(View.VISIBLE);
+
+        Task[] taskList = new Task[] {new Task("1", "Tarea numero uno", "María Gutierrez L", "Preguntar sobre la situación de transporte", new Date(), "Alta", "Presencial", "Susana Cornejo"), new Task("2", "Tarea numero dos", "Sandra Castro J", "Conseguir teléfono de persona de contacto", new Date(), "Media", "Telefónico", "Andres Cornejo"), new Task("3", "Tarea numero tres", "Lucía López Ruíz", "Acompañar a quimioterapia", new Date(), "Baja", "Presencial", "Susana Cornejo")};
+        this.getVisibleView(taskList);
+
+    }
+
+    private void getVisibleView(Task[] dayTasks) {
+        if (dayTasks.length > 0) {
+            RecyclerView recyclerView = findViewById(R.id.task_recycler);
+            recyclerView.setVisibility(View.VISIBLE);
+            TaskAdapter adapter = new TaskAdapter(dayTasks);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        } else {
+            RelativeLayout eventText = findViewById(R.id.no_events);
+            eventText.setVisibility(View.VISIBLE);
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/Task.java
@@ -0,0 +1,103 @@
+package com.pnp.galia_app_mobile;
+
+import java.util.Date;
+
+public class Task {
+    private String id;
+    private String name;
+    private String patient;
+    private String description;
+    private Date deadline;
+    private String priority;
+    private String mode;
+    private String asignTo;
+
+    public Task (String id, String name, String patient, String description, Date deadline, String priority, String mode, String asignTo) {
+        this.id = id;
+        this.name = name;
+        this.patient = patient;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.mode = mode;
+        this.asignTo = asignTo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(String patient) {
+        this.patient = patient;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getAsignTo() {
+        return asignTo;
+    }
+
+    public void setAsignTo(String asignTo) {
+        this.asignTo = asignTo;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", patient='" + patient + '\'' +
+                ", description='" + description + '\'' +
+                ", deadline=" + deadline +
+                ", priority='" + priority + '\'' +
+                ", mode='" + mode + '\'' +
+                ", asignTo='" + asignTo + '\'' +
+                '}';
+    }
+}
