8d260fb34324d1ff3312ccece3f43c6041ed4494
Tatiana Chacón Mora <tatumora1302@gmail.com>
2022-08-05 07:52:28 -0600

    RecyclerView Implement
    Se implementó el recyclerview en barreras. Falta por terminar.

--- app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/BarriersAllFragment.java
+++ app-mobile-android/app/src/main/java/com/pnp/galia_app_mobile/BarriersAllFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +29,7 @@ public class BarriersAllFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private RecyclerView recyclerView;
 
     public BarriersAllFragment() {
         // Required empty public constructor
@@ -61,7 +66,30 @@ public class BarriersAllFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_barriers_all, container, false);
+        recyclerView = myView.findViewById(R.id.barrier_recycler);
 
+        Barrier[] barrierList = new Barrier[] {new Barrier("1", "Ausencia de redes de apoyo", "B02", "Maria Gutierrez L", "Barrera numero uno", "Sociocultural", "Media", "Interna"), new Barrier("2", "Religión", "B07", "Lucía Brenes J", "Barrera numero dos", "Personal", "Alta", "Interna"), new Barrier("3", "Desestimación del caso", "B23", "Yolanda Rojas", "Barrera numero tres", "Serv.Salud", "Alta", "Externa"), new Barrier("4", "Variaciones del personal médico", "B27", "Miriam Ureña P", "Barrera numero cuatro", "Serv.Salud", "Baja", "Externa")};
+        getVisibleView(barrierList);
         return myView;
+    }
+
+    private void getVisibleView(Barrier[] listBarriers) {
+        if (listBarriers.length > 0) {
+            final float scale = getContext().getResources().getDisplayMetrics().density;
+            int multiTask = (int) (75 * scale + 0.5f);
+            int oneTask = (int) (156 * scale + 0.5f);
+            if (listBarriers.length == 1) {
+                recyclerView.getLayoutParams().height = multiTask;
+            }
+            else {
+                recyclerView.getLayoutParams().height = oneTask;
+            }
+            BarrierAdapter adapter = new BarrierAdapter(listBarriers);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        } else {
+            recyclerView.setVisibility(View.INVISIBLE);
+        }
     }
 }
